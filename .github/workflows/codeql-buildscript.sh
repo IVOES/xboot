7fdd6514feeac5741e0a2a7a74d98b81830ea152
Brian <89487381+b4yuan@users.noreply.github.com>
2024-02-07 14:46:00 -0500

    Update codeql-buildscript.sh

--- .github/workflows/codeql-buildscript.sh
+++ .github/workflows/codeql-buildscript.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
-sudo apt-get install -y libsdl2-dev libdrm-dev gcc-arm-linux-gnueabihf
 sudo apt-get update 
+sudo apt-get install -y libsdl2-dev libdrm-dev gcc-arm-linux-gnueabihf
 make
